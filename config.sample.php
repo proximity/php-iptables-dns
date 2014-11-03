83f0cba82a062b9c8d5bfa490f7305aa4d96641e
Dylan Jennings <dylan.jennings@proximity.co.nz>
2014-11-03 15:55:10 +1300

    Add new iptables config options

--- config.sample.php
+++ config.sample.php
@@ -1,7 +1,8 @@
 <?php
 $config = array(
 	'debug'				=> false,
-	'chain'				=> 'INPUT',
+	'iptables_chain'	=> 'INPUT',
+	'iptables_action'	=> 'ACCEPT',
 	'cache_location'	=> dirname(__FILE__) . '/.cache',
 	'hosts'				=> array(
 		'hostname' => 'Friendly name',
