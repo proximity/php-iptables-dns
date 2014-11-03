b0c6fe5b1b995c699da193864a95a9abaf1b1de2
Dylan Jennings <dylan.jennings@proximity.co.nz>
2014-11-03 15:55:43 +1300

    Add debugging + actual iptables function implementations

--- sync.php
+++ sync.php
@@ -2,11 +2,20 @@
 require_once('config.php');
 require_once('iptables.php');
 
+function debug($message)
+{
+	global $config;
+	if ($config['debug'])
+	{
+		echo $message . "\n";
+	}
+}
+
 // Put the current cache into an array so we can compare easily
 $cache = array();
 if (file_exists($config['cache_location']))
 {
-	$cache = json_decode(file_get_contents($config['cache_location']));
+	$cache = unserialize(file_get_contents($config['cache_location']));
 }
 
 // Make sure hosts exists in configuration
@@ -18,8 +27,11 @@ if (!isset($config['hosts']) || !is_array($config['hosts']))
 // Loop through hosts
 foreach($config['hosts'] as $host => $label)
 {
+	debug("\n----------");
+
 	// Get the IP of the host
 	$ip = gethostbyname($host);
+	debug("$host resolved to $ip");
 	$resolveFailed = ($ip === $host);
 	$add = true;
 
@@ -27,29 +39,36 @@ foreach($config['hosts'] as $host => $label)
 	if (isset($cache[$host]))
 	{
 		// If it's different, update iptables
+		debug("Cache hit");
 		$old = $cache[$host];
 
 		if ($old === $ip)
 		{
 			// IP has not changed. Don't delete, and don't add.
-			$add = false;
+			debug("IP has not changed, continue\n----------");
+			continue;
 		}
 		else
 		{
 			// Delete old rule
-			IPTables::delete($ip);
+			debug("IP changed, rule to be deleted");
+			IPTables::delete($ip, $config['iptables_action'], $config['iptables_chain']);
 		}
 	}
 
 	// Add new rule as long as resolve was successful
-	if (!$resolveFailed && $add)
+	if (!$resolveFailed)
 	{
-		IPTables::add($ip);
+		debug("Add new rule for $ip");
+		IPTables::add($ip, $config['iptables_action'], $config['iptables_chain']);
 	}
 
+	debug("Update cache");
 	// Update cache
 	$cache[$host] = $ip;
+
+	debug("----------");
 }
 
 // Write to cache
-file_put_contents($config['cache_location'], json_encode($cache));
+file_put_contents($config['cache_location'], serialize($cache));
