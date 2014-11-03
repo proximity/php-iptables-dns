6bbfb0ce6a397bc6590a4d5274a98fd291fc6587
Dylan Jennings <dylan.jennings@proximity.co.nz>
2014-11-03 15:42:15 +1300

    Update sample config with debug option

--- config.sample.php
+++ config.sample.php
@@ -1,8 +1,9 @@
 <?php
 $config = array(
+	'debug'				=> false,
 	'chain'				=> 'INPUT',
 	'cache_location'	=> dirname(__FILE__) . '/.cache',
-	'hosts'			=> array(
+	'hosts'				=> array(
 		'hostname' => 'Friendly name',
 	)
 );
