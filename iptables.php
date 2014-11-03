e1d145ee855c338b16d78973d50d4c2482025eea
Dylan Jennings <dylan.jennings@proximity.co.nz>
2014-11-03 15:55:24 +1300

    Implement iptables functions

--- iptables.php
+++ iptables.php
@@ -1,13 +1,15 @@
 <?php
 
 class IPTables {
-	static function add($chain, $ip)
+	static function add($ip, $action = 'ACCEPT', $chain = 'INPUT')
 	{
-
+		$ipEscaped = escapeshellarg($ip);
+		$cmd = "iptables -A $chain -s $ipEscaped -j $action";
 	}
 
-	static function delete($chain, $ip)
+	static function delete($ip, $action = 'ACCEPT', $chain = 'INPUT')
 	{
-
+		$ipEscaped = escapeshellarg($ip);
+		$cmd = "iptables -D $chain -s $ipEscaped -j $action";
 	}
 }
